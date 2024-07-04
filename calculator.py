8f6ffef1b0f659dbff185ebafccc246abc59b60e
Vanshikaa129 <vanshikachy01@gmail.com>
2024-07-04 16:25:29 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+A = float(input("Enter the first number :"))
+B = float(input("Enter the second number : "))
+opt = input("Enter the operator (+,-,*,/) : ")
+
+if opt == '+':
+    print(A+B)
+    
+elif opt == '-':
+    print(A-B)
+    
+elif opt == '*':
+    print(A*B)
+    
+elif opt == '/':
+    print (A/B)
